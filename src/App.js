e6f0ce1de4496f167c4a323140b3dabd3168643f
Asif Nawaz <assu.nawaz007@gmail.com>
2022-11-24 22:56:04 +0500

    added useState Hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
-
+import { useState } from 'react';
 
 function App() {
+
+  const [currentInput, setCurrentInput] = useState("");
+  const [inputList, setInputList] = useState([])
+
+  const handleChange = e => {
+    console.log(e.target.value);
+    setCurrentInput(e.target.value);
+  }
+
   return (
     <div className="App">
       <h1>Todo App</h1>
 
-    <input placeholder='Name' />
+    <input placeholder='Name' value={currentInput} onChange= {handleChange} />
+<br/> <br/>
+    <button className='addName' onClick={addNametoList}>ADD NAME</button>
+
 <br /><br />
-    <button className='addName'>ADD NAME</button>
+<h2>Name List</h2>
 
     </div>
   );
