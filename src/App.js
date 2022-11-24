3ae7b011b86d4941f9f4db3bcb28c2d2ea164ec3
Asif Nawaz <assu.nawaz007@gmail.com>
2022-11-25 02:37:51 +0500

    added update and delete

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
 
   const [currentName, setCurrentName] = useState("");
   const [nameToList, setNameToList] = useState([])
+  const [editIndex, setEditIndex] = useState("");
+  const [editStatus, setEditStatus] = useState("add");
+
 
   // OnChange for currentInput
   const handleChange = e => {
@@ -16,16 +19,38 @@ function App() {
 
   // Button onClick for addName to the List
 const addNametoList = () => {
-  setNameToList([...nameToList, currentName]);
+console.log("add some Function...")
+  if(currentName === "") return null 
+
+  if(editStatus === "edit") {
+    let temp = [...nameToList]
+    temp.splice(editIndex, 1, currentName)
+    console.log("temp", temp);
+    setNameToList([...temp])
+  }else {
+    console.log("add...");
+    setNameToList([...nameToList, currentName]);
+  }
   setCurrentName("");
+  setEditIndex("");
+  setEditStatus("add");
 }
 
 // for edit button 
-const editCurrentName = (userName) => {
+const editCurrentName = (userName, index) => {
 console.log(userName);
 setCurrentName(userName);
+setEditIndex(index)
+setEditStatus("edit")
 }
 
+
+// for delete 
+const deleteName= (index) => {
+  let temp = [...nameToList]
+  temp.splice(index,1)
+  setNameToList(temp)
+}
 
 console.log("NameList", nameToList);
 
@@ -46,11 +71,13 @@ console.log("NameList", nameToList);
 <div>
 <h2><u>Name List</u></h2>
 {
-  nameToList ?.length && nameToList.map((userName, i) => {
+  nameToList?.length && nameToList.map((userName, i) => {
     return (
        <div className='row'>
-      <span>{i + 1 + " " + userName}
-      <button className='btnLeftM' onClick={() => editCurrentName(userName)}>Edit</button> </span>
+      <span>{i + 1 + " " + userName}</span>
+      <button className='btnLeftM' onClick={() => editCurrentName(userName, i)}>Edit</button> 
+      <button className='btnLeftM' onClick={() => deleteName(i)}>Delete</button> 
+      
       </div>
       
     ) 
