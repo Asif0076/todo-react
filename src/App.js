be98052ea727276a9fe703d355ebd035ef2a383b
Asif Nawaz <assu.nawaz007@gmail.com>
2022-11-25 00:35:46 +0500

    added edit button

--- src/App.js
+++ src/App.js
@@ -4,24 +4,61 @@ import { useState } from 'react';
 
 function App() {
 
-  const [currentInput, setCurrentInput] = useState("");
-  const [inputList, setInputList] = useState([])
+  const [currentName, setCurrentName] = useState("");
+  const [nameToList, setNameToList] = useState([])
 
+  // OnChange for currentInput
   const handleChange = e => {
     console.log(e.target.value);
-    setCurrentInput(e.target.value);
+    setCurrentName(e.target.value);
+    
   }
+
+  // Button onClick for addName to the List
+const addNametoList = () => {
+  setNameToList([...nameToList, currentName]);
+  setCurrentName("");
+}
+
+// for edit button 
+const editCurrentName = (userName) => {
+console.log(userName);
+setCurrentName(userName);
+}
+
+
+console.log("NameList", nameToList);
 
   return (
     <div className="App">
       <h1>Todo App</h1>
 
-    <input placeholder='Name' value={currentInput} onChange= {handleChange} />
+    <input placeholder='Name' value={currentName} onChange= {handleChange} />
 <br/> <br/>
     <button className='addName' onClick={addNametoList}>ADD NAME</button>
 
 <br /><br />
-<h2>Name List</h2>
+
+
+
+
+<div className='nameList'>
+<div>
+<h2><u>Name List</u></h2>
+{
+  nameToList ?.length && nameToList.map((userName, i) => {
+    return (
+       <div className='row'>
+      <span>{i + 1 + " " + userName}
+      <button className='btnLeftM' onClick={() => editCurrentName(userName)}>Edit</button> </span>
+      </div>
+      
+    ) 
+  })
+}
+</div>
+</div>
+
 
     </div>
   );
---
+++ src/components/EditButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "/components/EditButton.css"
+
+function EditButton() {
+  return (
+    <div>EditButton</div>
+  )
+}
+
+export default EditButton
